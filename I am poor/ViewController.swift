2036691bd9e36429474ee0252a49083df90aec2e
collins ihezie <collinsihezie6@gmail.com>
2022-07-27 11:44:23 +0100

    initial commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Gava Developer on 24/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
